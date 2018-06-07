1c3ce69d001884348063b53786ce45ae1045a4bc
LilliamHaro <qualia950@gmail.com>
2018-06-07 11:33:08 -0500

    subiendo primer submenu

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
 $(document).ready(function () {
+    $('#first-click').on('click', function(event) {
+        $('#team').html('<li id="jp"><strong>Juan Perez</strong><div class="fz-10">Presidente</div></li> <li id="mr"><strong>Mario Rosas</strong><div class="fz-10">Gerente General</div></li> <li id="mr" class="mr-b-9"><strong>Christiano Chavez</strong></li><li id="mb" class="mr-b-9"><strong>Mariella Benites</strong></li>')
     
+       
+    
+    
+    })
+
 });
