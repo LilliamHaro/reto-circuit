1e3bead2e9917c570d066f3687e9b066ee66cf0c
LilliamHaro <qualia950@gmail.com>
2018-06-07 12:00:06 -0500

    subiendo segundo submenu

--- js/app.js
+++ js/app.js
@@ -1,10 +1,13 @@
 $(document).ready(function () {
-    $('#first-click').on('click', function(event) {
-        $('#team').html('<li id="jp"><strong>Juan Perez</strong><div class="fz-10">Presidente</div></li> <li id="mr"><strong>Mario Rosas</strong><div class="fz-10">Gerente General</div></li> <li id="mr" class="mr-b-9"><strong>Christiano Chavez</strong></li><li id="mb" class="mr-b-9"><strong>Mariella Benites</strong></li>')
-    
-       
-    
-    
-    })
+    $('#first-click').on('click', function (event) {
+        $('#team').html('<li data-id="jp"><strong>Juan Perez</strong><div class="fz-10">Presidente</div></li> <li data-id="mr"><strong>Mario Rosas</strong><div class="fz-10">Gerente General</div></li> <li data-id="mr" class="mr-b-9"><strong>Christiano Chavez</strong></li><li data-id="mb" class="mr-b-9"><strong>Mariella Benites</strong></li>')
+        $('#team li').on('click', function (event) {
+            var idMember = $(this).data('id');
+            if (idMember == 'jp') {
+                $('#info').text('jp jp jp jp jp')
+            }
+        });
+    });
+
 
 });
